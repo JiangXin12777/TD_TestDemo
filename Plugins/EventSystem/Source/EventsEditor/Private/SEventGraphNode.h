6dbd5d7b4e3d4b7f5f931fd81e9556fc3100c8b2
WIN-ST5EU1T38E7\Administrator <2649340476@qq.com>
2025-01-17 17:53:35 +0800

    ci: Commit EventSystem plugin.

---
+++ Plugins/EventSystem/Source/EventsEditor/Private/SEventGraphNode.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "KismetNodes/SGraphNodeK2Base.h"
+
+class UEventsK2Node_EventBase;
+
+/**
+ * 
+ */
+class EVENTSEDITOR_API SEventGraphNode : public SGraphNodeK2Base
+{
+public:
+	SLATE_BEGIN_ARGS(SEventGraphNode) {}
+	SLATE_END_ARGS()
+
+	void Construct(const FArguments& InArgs, UEventsK2Node_EventBase* InNode);
+
+
+public:
+	virtual void CreatePinWidgets() override;
+};
